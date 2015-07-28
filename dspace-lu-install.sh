3b6d4218ac8f99ad2776fe817f5b987906f845dc
Victoria Lacroix <victoria.a.lacroix@gmail.com>
2015-07-28 12:25:55 -0400

    Updated installation instructions.
    Signed-off-by: Victoria Lacroix <victoria.a.lacroix@gmail.com>

--- dspace-lu-install.sh
+++ dspace-lu-install.sh
@@ -23,11 +23,15 @@
 # This script is currently intended as more of a guide, rather than an automation of
 # the installation process, although under the right conditions it should work.
 
+# This script assumes that the version of Tomcat that the server is running is Tomcat 7.
 
 
 DSPACE_SRC='/path/to/dspace/src'
-DSPACE_DB_BACKUP='dspace_database.dump'
+DSPACE_DB_BACKUP='/path/to/dspace_database.dump'
 ASSETSTORE_BACKUP='/path/to/assetstore'
+
+cp -r modules/ ${DSPACE_SRC}/dspace/
+cp -r config/ ${DSPACE_SRC}/dspace/
 
 cd ${DSPACE_SRC}/dspace/
 
@@ -35,19 +39,19 @@ cd ${DSPACE_SRC}/dspace/
 mvn -U clean package
 
 # Stop Tomcat, remove the old version of DSpace
-sudo /etc/init.d/tomcat6 stop
-sudo rm -Rf /dspace
+sudo systemctl stop tomcat7
+sudo rm -rf /dspace
 
 # Delete and create empty database for fresh DSpace install
 sudo su -c "dropdb dspace ; createdb -U dspace -E UNICODE dspace" postgres
 
 # Install DSpace
-cd target/dspace-1.7.2-build.dir/
+cd target/dspace-installer/
 sudo ant fresh_install
 
 # Copy over the backed up assetstore
-sudo rm -Rf /dspace/assetstore
-sudo cp -R ${ASSETSTORE_BACKUP} /dspace/
+sudo rm -rf /dspace/assetstore
+sudo cp -r ${ASSETSTORE_BACKUP} /dspace/
 
 # Recreate the database with backed up version
 sudo su -c "cd /var/lib/postgresql && dropdb dspace && createdb -U dspace -E UNICODE dspace && psql dspace < ${DSPACE_DB_BACKUP}" postgres
@@ -57,14 +61,14 @@ sudo ln -s /dspace/webapps/oai /dspace/webapps/dspace-oai
 sudo ln -s /dspace/webapps/jspui /dspace/webapps/dspace
 
 # Set permissions for /dspace
-sudo chown -R tomcat6:root /dspace
+sudo chown -R tomcat7:tomcat7 /dspace
 
 # Prepare the indexes, media
 sudo /dspace/bin/dspace index-init
-sudo /etc/init.d/tomcat6 start
+sudo systemctl restart tomcat7
 sudo /dspace/bin/dspace filter-media
 
 # Done
-echo "----------------------"
-echo "Done installing DSpace..."
+echo "-----------------------"
+echo "Done installing DSpace."
 
